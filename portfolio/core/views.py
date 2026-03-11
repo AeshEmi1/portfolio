26921d28b4913aa0702edb760cc4081ef331af94
AeshEmi1 <121838259+AeshEmi1@users.noreply.github.com>
2026-03-10 21:06:32 -0500

    [feat] adds nginx configuration and cache control headers to the views

--- portfolio/core/views.py
+++ portfolio/core/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_control
+from django.utils.decorators import method_decorator
 
 
+@method_decorator(
+    cache_control(public=True, max_age=600),
+    name="dispatch",
+)
 def index(request):
     return render(request, "index.html")
 
 
+@method_decorator(
+    cache_control(public=True, max_age=600),
+    name="dispatch",
+)
 def projects(request):
     return render(request, "projects.html")
 
 
+@method_decorator(
+    cache_control(public=True, max_age=600),
+    name="dispatch",
+)
 def contact(request):
     return render(request, "contact.html")
